5d94170df5a32d67fa8969d39e56b2f87a620fc9
Firstio Anmar <48433601+firstioanmar@users.noreply.github.com>
2023-11-06 16:07:10 +0700

    frontend update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="login-box">
         <div class="login-logo">
-            <img src="{{ asset('img/logo_tib.png') }}" style="width: 50%; height: auto;">
+            <img src="{{ asset('img/tib_logo_outline.png') }}" style="width: 50%; height: auto;">
         </div>
         <!-- /.login-logo -->
         <div class="card">
@@ -11,41 +11,47 @@
 
                 <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
                     @csrf
-                    <div class="form-group row">
-                        <div class="input-group mb-3">
-                            <input id="username" type="text"
-                                class="form-control {{ $errors->has('email') || $errors->has('username') ? ' is-invalid' : '' }}"
-                                name="username" value="{{ old('username') }}" required autofocus placeholder="Username">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span class="fas fa-user"></span>
-                                </div>
-                            </div>
-                        </div>
-                        @error('username')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="form-group row">
-                        <div class="input-group mb-3">
-                            <input id="password" type="password"
-                                class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
-                                required placeholder="Password">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span id="togglePassword" class="fas fa-eye"></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div class="row">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <input id="username" type="text"
+                                        class="form-control {{ $errors->has('email') || $errors->has('username') ? ' is-invalid' : '' }}"
+                                        name="username" value="{{ old('username') }}" required autofocus
+                                        placeholder="Username">
+                                    <div class="input-group-append bg-gray text-white">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-user text-light" style="width: 14px"></span>
+                                        </div>
+                                    </div>
+                                    @error('username')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <input id="password" type="password"
+                                        class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                        name="password" required placeholder="Password">
+                                    <div class="input-group-append bg-gray text-white">
+                                        <div class="input-group-text">
+                                            <span id="togglePassword" class="fas fa-eye text-light"
+                                                style="width: 14px"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <!-- /.col -->
                         <div class="col-12">
                             <button type="submit" class="btn btn-primary btn-block text-uppercase">Login</button>
