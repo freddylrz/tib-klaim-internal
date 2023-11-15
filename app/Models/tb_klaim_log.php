c6ec1ad490caee9a2f720aa240c3b69c2f8d8835
faizalajik <faizalajikurniawan@gmail.com>
2023-11-15 15:53:47 +0700

    add insert claim

---
+++ app/Models/tb_klaim_log.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tb_klaim_log extends Model
+{
+    protected $table = 'klaimapps_db.tb_klaim_log';
+//    protected $connection = 'mysql2';
+}
---
+++ app/Models/tb_uploads.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class tb_uploads extends Model
+{
+    protected $table = 'klaimapps_db.tb_uploads';
+//    protected $connection = 'mysql2';
+}
